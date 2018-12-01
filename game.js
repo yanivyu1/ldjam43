78238fa8d6d50ff8d2443c7b29dc4e194d0553e6
Yoni <yoni@guardicore.com>
2018-12-01 14:14:13 +0200

    Improved walls and floors

--- game.js
+++ game.js
@@ -24,29 +24,21 @@ var level = {
             .image('assets/background.png');
 
         Crafty.viewport.zoom(1/window.devicePixelRatio, 0, 0, 0);
-
-        Crafty.c('Floor', {
-            init: function() {
-                this.addComponent('2D, DOM, blocking, Image, Solid');
-                this.image('assets/floor.png', 'repeat');
-            }
-        });
         
         Crafty.e('2D, DOM, Character, character_start, SpriteAnimation, Twoway, Gravity, Collision')
             .attr({x: consts.tile_width, y: consts.tile_height})
             .reel("walking", 1000/12*5, [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]])
             .animate("walking", -1)
             .twoway(200)
-            .gravity("blocking")
-            .checkHits('Solid')
-            .bind('HitOn', this.characterHit);
+            .gravity("gravity_blocking")
+            .bind('Move', this.characterMoved);
         
         this.addFloor(0, 19, 30);
         this.addFloor(0, 16, 5);
 
-        for (var i = 0; i < consts.level_height; i++) {
-            this.addFloor(0, i, 1);
-            this.addFloor(consts.level_width - 1, i, 1);
+        for (var i = 0; i < consts.level_height - 1; i++) {
+            this.addWall(0, i, 1);
+            this.addWall(consts.level_width - 1, i, 1);
         }
     },
 
@@ -56,12 +48,40 @@ var level = {
             .attr({x: tile_x * 32, y: tile_y * 32, w: tile_width * 32, h: 32});
     },
 
-    characterHit: function(hitData)
+    addWall: function(tile_x, tile_y, tile_width)
     {
-        Crafty.log('Collision with Solid');
-        Crafty.log(hitData);
+        Crafty.e('Wall')
+            .attr({x: tile_x * 32, y: tile_y * 32, w: tile_width * 32, h: 32});
+    },
+
+    characterMoved: function(evt)
+    {
+        if (this.fixing_position) return;
+
+        if (this.hit('move_blocking')) {
+            this.fixing_position = true;
+            this.x = evt._x;
+            this.fixing_position = false;
+        }
     }
 };
+
+function initComponents()
+{
+    Crafty.c('Floor', {
+        init: function() {
+            this.addComponent('2D, DOM, gravity_blocking, Image');
+            this.image('assets/floor.png', 'repeat');
+        }
+    });
+
+    Crafty.c('Wall', {
+        init: function() {
+            this.addComponent('2D, DOM, Image, move_blocking');
+            this.image('assets/wall.png', 'repeat');
+        }
+    })
+}
 
 function initGame()
 {
@@ -69,6 +89,7 @@ function initGame()
     //Crafty.viewport.zoom(1/window.devicePixelRatio, 0, 0, 0);
 
     Crafty.load(assets, function() {
+        initComponents();
         level.render();
     });
 }
