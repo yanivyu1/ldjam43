c5785f7c70d035270f5f8d9ed842d776842284e8
Yoni <yoni@guardicore.com>
2018-12-01 13:17:33 +0200

    More stuff added

--- game.js
+++ game.js
@@ -10,6 +10,13 @@ var assets = {
     }
 };
 
+var consts = {
+    tile_width: 32,
+    tile_height: 32,
+    level_width: 30,
+    level_height: 20
+};
+
 var level = {
     render: function() {
         Crafty.e('2D, DOM, Image')
@@ -17,17 +24,42 @@ var level = {
             .image('assets/background.png');
 
         Crafty.viewport.zoom(1/window.devicePixelRatio, 0, 0, 0);
+
+        Crafty.c('Floor', {
+            init: function() {
+                this.addComponent('2D, DOM, blocking, Image, Solid');
+                this.image('assets/floor.png', 'repeat');
+            }
+        });
         
-        Crafty.e('2D, DOM, character_start, SpriteAnimation, Twoway, Gravity')
-            .attr({x: 0, y: 0})
+        Crafty.e('2D, DOM, Character, character_start, SpriteAnimation, Twoway, Gravity, Collision')
+            .attr({x: consts.tile_width, y: consts.tile_height})
             .reel("walking", 1000/12*5, [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]])
             .animate("walking", -1)
             .twoway(200)
-            .gravity("platform");
+            .gravity("blocking")
+            .checkHits('Solid')
+            .bind('HitOn', this.characterHit);
+        
+        this.addFloor(0, 19, 30);
+        this.addFloor(0, 16, 5);
 
-        Crafty.e('2D, DOM, platform, Color')
-            .attr({x: 0, y: 540, w: 960, h:100})
-            .color('#F0F');
+        for (var i = 0; i < consts.level_height; i++) {
+            this.addFloor(0, i, 1);
+            this.addFloor(consts.level_width - 1, i, 1);
+        }
+    },
+
+    addFloor: function(tile_x, tile_y, tile_width)
+    {
+        Crafty.e('Floor')
+            .attr({x: tile_x * 32, y: tile_y * 32, w: tile_width * 32, h: 32});
+    },
+
+    characterHit: function(hitData)
+    {
+        Crafty.log('Collision with Solid');
+        Crafty.log(hitData);
     }
 };
 
